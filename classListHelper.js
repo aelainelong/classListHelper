ecc0458fad66a13efa81764b36d48566cd636879
Ashley Long <a.elainelong@gmail.com>
2018-01-26 14:35:07 -0600

    Refactor/rewrite

--- classListHelper.js
+++ classListHelper.js
@@ -1,41 +1,36 @@
-// Cross-browser Class Methods Function
-function classes(elem, action, theClass) {
-  if(elem.classList){
-    switch(action){
-      case 'add':
-        elem.classList.add(theClass);
-        break;
-      case 'remove':
-        elem.classList.remove(theClass);
-        break;
-      case 'toggle':
-        elem.classList.toggle(theClass);
-        break;
-      case 'contains':
-        return elem.classList.contains(theClass) ? true : false;
-        break;
-      default:
-        console.log("No class actions matched.");
-    }
-  } else {
-    switch(action){
-      case 'add':
-        elem.className += ' ' + theClass;
-        break;
-      case 'remove':
-        elem.className = elem.className.replace(new RegExp('(^|\\b)' + theClass.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-        break;
-      case 'toggle':
-        const classes = elem.className.split(' ');
-        const existingIndex = classes.indexOf(theClass);
-        existingIndex >= 0 ? classes.splice(existingIndex, 1) : classes.push(theClass);
-        elem.className = classes.join(' ');
-        break;
-      case 'contains':
-        return (elem.className.indexOf(theClass) > -1) ? true : false;
-        break;
-      default:
-        console.log("No class actions matched.");
+/*
+  * classListHelper.js - A light-weight cross-browser script for adding, removing, toggling and checking element class names.
+  * v2.0 - by AElaineLong
+  * https://github.com/aelainelong/classListHelper
+*/
+(function(){
+  "use strict";
+
+  // Add a class
+  Node.prototype.addClass = function(myClass){
+    this.classList ? this.classList.add(myClass) : this.className += ' ' + myClass;
+  }
+  // Remove a class
+  Node.prototype.removeClass = function(myClass){
+    this.classList ? this.classList.remove(myClass) : this.className = elem.className.replace(new RegExp('(^|\\b)' + myClass.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }
+  // Toggle a class
+  Node.prototype.toggleClass = function(myClass){
+    if (this.classList) {
+      this.classList.toggle(myClass);
+    } else {
+      var classes = this.className.split(' ');
+      var existingIndex = classes.indexOf(myClass);
+      existingIndex >= 0 ? classes.splice(existingIndex, 1) : classes.push(myClass);
+      this.className = classes.join(' ');
     }
   }
-}
+  // Check if element contains a class, returns boolean
+  Node.prototype.containsClass = function(myClass){
+    if (this.classList) {
+      return this.classList.contains(myClass) ? true : false;
+    } else {
+      return (this.className.indexOf(myClass) > -1) ? true : false;
+    }
+  }
+})();
